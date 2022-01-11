bcb169ec8faa4795a2114077656114a93131dccb
Marcos Nascimento <marcosnascimento.dev@gmail.com>
2022-01-11 00:36:13 -0300

    Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,9 +6,22 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            var funcionarios = new Funcionario[] { new Funcionario() { Id = 200, Name = "Marcos" } };
+
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.Id);
+                Console.WriteLine(funcionario.Name);
+            }
 
         }
+    }
 
+    struct Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
 
